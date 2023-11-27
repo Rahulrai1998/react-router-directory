b6d4fc80de1c1ad29e7f6a9d8ec0d00f6f29822d
Rahul Kumar <rahul.kumar@stockedge.com>
2023-11-27 12:31:54 +0530

    new approach applied

--- jobarouter/src/App.js
+++ jobarouter/src/App.js
@@ -1,11 +1,29 @@
+import {
+  Route,
+  Routes,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 
+//pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+//layouts
+import RouteLayout from "./layouts/RouteLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RouteLayout />}>
+      <Route index element={<Home />}></Route>
+      <Route path="about" element={<About />}></Route>
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <div>
-      <p>Hello, Folks</p>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
